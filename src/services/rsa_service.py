68418e5092926533415ba4b5ad9136a24d41a85c
Doubleneck <antti.vuorenmaa@kolumbus.fi>
2022-07-24 20:57:21 +0300

    clean code rsa_service

--- src/services/rsa_service.py
+++ src/services/rsa_service.py
@@ -36,31 +36,29 @@ class RsaService:
                 is_prime = False
         return is_prime
 
-    def miller_rabin_check(self,n):
+    def miller_rabin_check(self,n_val):
         '''testaa onko annettu luku suurella todennäköisyydellä alkuluku'''
         exp = 0
-        a = n-1
-        while a % 2 == 0:
-            a = a//2
+        a_val = n_val-1
+        while a_val % 2 == 0:
+            a_val = a_val // 2
             exp += 1
-        assert 2**exp * a == n-1
-        m = a
-    # print("exp", exp)
-    # print("m",m)
-        a_for_test = random.randrange(2, n-1)
+        assert 2**exp * a_val == n_val-1
+        m_val = a_val
+        a_for_test = random.randrange(2, n_val-1)
 
         def test_unit(a_for_test):
             '''yksittäinen miller-rabin testi'''
-            if pow(a_for_test, m, n) == 1 or pow(a_for_test, m, n) == n-1:
+            if pow(a_for_test, m_val, n_val) == 1 or pow(a_for_test, m_val, n_val) == n_val-1:
                 return False # a_for_test luultavasti ON alkuluku.
             for i in range(1,exp):
-                if pow(a_for_test, 2**i * m, n) == n-1:
+                if pow(a_for_test, 2**i * m_val, n_val) == n_val-1:
                     return False
             return True #  a_for_test varmasti EI ole alkuluku.
 
-        #20 testiä eri a:n arvoilla:
-        for i in range(20):
-            a_for_test  = random.randrange(2, n)
+        #40 testiä eri a:n arvoilla:
+        for i in range(40):
+            a_for_test  = random.randrange(2, n_val)
             if test_unit(a_for_test):
                 return False
         return True
@@ -82,79 +80,56 @@ class RsaService:
                 found = False
         return low_level_candidate
 
-    def create_q(self,p):
+    def create_q(self,p_value):
         '''luo alkuluvun q joka on p:n co-prime'''
         found = False
         while not found:
             candidate = self.n_odd_random(512)
             low_level_candidate = self.find_low_level_candidate(candidate)
             found = self.miller_rabin_check(low_level_candidate)
-            if found and low_level_candidate == p:
+            if found and low_level_candidate == p_value:
                 found = False
             if low_level_candidate % 65537 == 0:
                 found = False
         return low_level_candidate
 
-    def create_keys(self):     
+    def create_keys(self):
+        '''luo avainparin, julkisen ja yksityisen avaimen'''
         p_value = self.create_p()
         q_value = self.create_q(p_value)
-        big_N = p_value * q_value
+        big_n = p_value * q_value
         r_value = (p_value-1) * (q_value-1)
         e_value = 65537
         d_value = self.extended_eucleidian(e_value, r_value)[0]
-        if d_value < 0: 
+        if d_value < 0:
             d_value += r_value
 
-        return (d_value,e_value,big_N)
-   # print("publicKey ", (e,N))
-   # print("privateKey ", (d,N))    
+        return (d_value,e_value,big_n)
 
-    def en_crypt(self,e,N,msg):
-        ret = pow(msg,e,N)
+    def en_crypt(self,e_val,n_val,msg):
+        '''suorittaa salauksen'''
+        ret = pow(msg,e_val,n_val)
         return ret
 
-    def de_crypt(self,d,N,msg):
-        ret = pow(msg,d,N)  
-        return ret 
+    def de_crypt(self,d_val,n_val,msg):
+        '''suorittaa purkamisen'''
+        ret = pow(msg,d_val,n_val)
+        return ret
 
-    def extended_eucleidian(self,a,b):
-        s = 0
+    def extended_eucleidian(self,a_val,b_val):
+        '''tuottaa d-komponentin yksityiseen avaimeen'''
+        s_val = 0
         old_s = 1
-        r = b
-        old_r = a
-        while r != 0:
-            quotient = old_r // r
-            old_r, r =  r, old_r - quotient * r
-            old_s, s =  s, old_s - quotient * s
-        if b != 0:
-            bezout_t = (old_r - old_s * a)	 // b
+        r_val = b_val
+        old_r = a_val
+        while r_val != 0:
+            quotient = old_r // r_val
+            old_r, r_val =  r_val, old_r - quotient * r_val
+            old_s, s_val =  s_val, old_s - quotient * s_val
+        if b_val != 0:
+            bezout_t = (old_r - old_s * a_val)	 // b_val
         else:
-            bezout_t = 0  
-        return old_s, bezout_t, old_r    
+            bezout_t = 0
+        return old_s, bezout_t, old_r
         # output "Bézout coefficients:", (old_s, bezout_t)
         # output "greatest common divisor:", old_r
-'''
-    def eucalgVANHA(self, a, b):
-        # make a the bigger one and b the lesser one
-        swapped = False
-        if a < b:
-            a, b = b, a
-            swapped = True
-        # ca and cb store current a and b in form of
-        # coefficients with initial a and b
-        # a' = ca[0] * a + ca[1] * b
-        # b' = cb[0] * a + cb[1] * b
-        ca = (1, 0)
-        cb = (0, 1)
-        while b != 0:
-            # k denotes how many times number b
-            # can be substracted from a
-            k = a // b
-            # a  <- b
-            # b  <- a - b * k
-            # ca <- cb
-            # cb <- (ca[0] - k * cb[0], ca[1] - k * cb[1])
-            a, b, ca, cb = b, a-b*k, cb, (ca[0]-k*cb[0], ca[1]-k*cb[1])
-        if swapped:
-            return (ca[1], ca[0])    
-'''
