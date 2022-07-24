c03d55af47fd50a075649572e170b13e119ae838
Doubleneck <antti.vuorenmaa@kolumbus.fi>
2022-07-24 20:35:03 +0300

    add .pylintrc

--- src/tests/conversion_service_test.py
+++ src/tests/conversion_service_test.py
@@ -8,9 +8,9 @@ class TestConversionService(unittest.TestCase):
 
     def test_char_a_equals_97(self):
         a_bin = self.conversionService.encode_str_to_bin("a")
-        self.assertEqual(a_bin, 97)    
+        self.assertEqual(a_bin, 97)
 
     def test_int_97_equals_char_a(self):
         a_str = self.conversionService.encode_bin_to_string(97)
-        self.assertEqual(a_str, "a")      
+        self.assertEqual(a_str, "a")
         
