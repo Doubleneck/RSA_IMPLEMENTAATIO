73ecac9adfe9a08fd0bab632a90ca252334e0173
Doubleneck <antti.vuorenmaa@kolumbus.fi>
2022-07-27 00:01:06 +0300

    add input validation in ui

--- src/ui/ui.py
+++ src/ui/ui.py
@@ -16,7 +16,6 @@ class UI:
             if select not in ["1","2","3","q"]:
                 print("Väärä valinta...yritä uudelleen \n")
             if select == "1":
-              #  print("GENEROIDAAN / DEMO ) \n")   
                 rsa_keys = RsaService().create_keys()
                 pub_key = str(rsa_keys[1])+"-"+str(rsa_keys[2])
                 pri_key = str(rsa_keys[0])+"-"+str(rsa_keys[2])
@@ -27,35 +26,43 @@ class UI:
                 print(pri_key,"\n")
 
             if select == "2":
-              #  print("SALATAAN / DEMO \n")   
                 while True:
                     pub_key = input("Anna julkinen avain: \n")
-                    e_value = int(pub_key.split("-")[0])
-                    n_value = int(pub_key.split("-")[1])
-                    message = input("Kirjoita viesti: \n")
-                    msg_bin = ConversionService().encode_str_to_bin(message)
-                    if msg_bin.bit_length() > 1022:
-                        print("viesti on liian pitkä salattavaksi! \n")
-                        print(msg_bin.bit_length())
-                        break
+                    try:
+                        split_key = pub_key.split("-",1)
+                        e_value = int(split_key[0])
+                        n_value = int(split_key[1])
+                        message = input("\nKirjoita viesti: \n")
+                        msg_bin = ConversionService().encode_str_to_bin(message)
+                        if msg_bin.bit_length() > 1022:
+                            print("Viesti on liian pitkä salattavaksi! \n")
+                            break
+                    except:
+                        print("Avain ei kelpaa!")
+                        continue
                     encrypted_msg = RsaService().en_crypt(e_value, n_value, msg_bin)
                     print("**********************************************\n")
                     print("viesti salattuna:\n")
                     print(encrypted_msg)
-                    
+                    print("\n")
                     break
 
             if select == "3":
                 while True:
                     pri_key = input("Anna yksityinen avain: \n")
-                    d_value = int(pri_key.split("-")[0])
-                    n_value = int(pri_key.split("-")[1])
-                    crypted_message = input("Anna salattu viesti: \n")
-                    decrypted_msg = RsaService().de_crypt(d_value, n_value, int(crypted_message))
-                    msg_str = ConversionService().encode_bin_to_string(decrypted_msg)
-                    print("**********************************************\n")
-                    print("viesti purettuna:\n")
-                    print(msg_str)
-                    print("**********************************************\n")
-                    
-                    break 
+                    try:
+                        split_key = pri_key.split("-",1)
+                        d_value = int(split_key[0])
+                        n_value = int(split_key[1])
+                        crypted_message = input("\nAnna salattu viesti: \n")
+                        decrypted_msg = RsaService().de_crypt(d_value, n_value, int(crypted_message))
+                        msg_str = ConversionService().encode_bin_to_string(decrypted_msg)
+                        print("**********************************************\n")
+                        print("viesti purettuna:\n")
+                        print(msg_str)
+                        print("\n")
+                        print("**********************************************\n")
+                        break
+                    except:
+                        print("Avain ei kelpaa!")
+                        continue
