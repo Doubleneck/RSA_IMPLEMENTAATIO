77adf9585b234a813ccd78e82112aa9ae4394786
Doubleneck <antti.vuorenmaa@kolumbus.fi>
2022-08-14 20:45:11 +0300

    add long test millerrabinxtimes.py

---
+++ src/millerrabinxtimes.py
@@ -0,0 +1,20 @@
+'''testiluokka miller-rabinin laajemmalle testille'''
+import sympy
+from services.rsa_service import RsaService
+
+def main():
+    '''testaa miller-rabinia n kertaa 1024 bit random -komposiitilla'''
+    n_value = 100
+    test_passed = True
+    for _ in range (n_value):
+        a_value = sympy.randprime(pow(2,511), pow(2,512))
+        b_value = sympy.randprime(pow(2,511), pow(2,512))
+        if RsaService().miller_rabin_check(a_value * b_value) is True:
+            print("Miller-rabin teki virheen syötteellä: \n", a_value * b_value)
+            test_passed = False
+            break
+    if test_passed:
+        print("Miller Rabin tuotti oikean tuloksen joka kerta. Testattiin ",n_value,"kertaa")
+
+if __name__ == '__main__':
+    main()
