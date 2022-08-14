011434feddd7581965f76fe9df04aa06ecb257e9
Doubleneck <antti.vuorenmaa@kolumbus.fi>
2022-08-14 20:58:00 +0300

    add tasks long test for miller-rabin

--- tasks.py
+++ tasks.py
@@ -13,6 +13,10 @@ def manual_test(ctx):
     ctx.run("python3 src/iterations.py")
 
 @task
+def mr_test(ctx):
+    ctx.run("python3 src/millerrabinxtimes.py")
+
+@task
 def coverage(ctx):
     ctx.run("coverage run --branch -m pytest", pty=True)
 
